025438c7e305ca060fe61cde93572e711135b167
Igal Shapira <igal.shapira@gmail.com>
2016-08-06 12:22:02 +0300

    Fix semester name

--- migrations/1469038260014-AddSemesters2017.js
+++ migrations/1469038260014-AddSemesters2017.js
@@ -4,14 +4,14 @@ var db = require('../app/db');
 exports.up = function(next) {
   db.Semester.create([
   	{
-  		name: 'סתיו תשע"ו', year: 2017, semester: 1,
+  		name: 'סתיז תשע"ז', year: 2017, semester: 1,
   		startDate: new Date("2016-10-29T00:00:00.000Z"),
   		endDate: new Date("2017-01-26T00:00:00.000Z"),
   		examsStart: new Date("2017-01-28T00:00:00.000Z"),
   		examsEnd: new Date("2017-03-09T00:00:00.000Z")
   	},
 	{
-	    name: 'אביב תשע"ו',  year: 2017, semester: 2,
+	    name: 'אביב תשע"ז',  year: 2017, semester: 2,
 	    startDate: new Date("2017-03-11T00:00:00.000Z"),
   		endDate: new Date("2017-06-29T00:00:00.000Z"),
   		examsStart: new Date("2017-07-01T00:00:00.000Z"),
