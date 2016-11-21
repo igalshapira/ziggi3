e164d50ebbe24eb721bfd3e1ebed3d7f5aad5b45
Tom weinberg <tomweinberg@users.noreply.github.com>
2016-11-21 19:58:05 +0200

    Update db.js

--- app/db.js
+++ app/db.js
@@ -1,24 +1,22 @@
-module.exports = function (app) {
-  var mongoose = require('mongoose');
-  var config = app.get('config');
+var mongoose = require('mongoose');
+  var config = require('./config');
   var Schema = mongoose.Schema;
   var dbUrl = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || ('mongodb://localhost/ziggi3_' + config.university)
 
-  mongoose.connect(dbUrl, function (err, res) {
+    mongoose.createConnection(dbUrl, function (err, res) {
       if (err) {
         console.log ('ERROR connecting to: ' + dbUrl + '. ' + err);
       } else {
         console.log ('Succeeded connected to: ' + dbUrl);
     }
   });
-
-  return {
+  module.exports= {
     mongoose: mongoose,
     Course: require('./model/course')(mongoose),
     Search: require('./model/search')(mongoose),
     Semester: require('./model/semester')(mongoose),
     Building: require('./model/building')(mongoose),
-    Department: require('./model/department')(mongoose),
+   Department: require('./model/department')(mongoose),
     Calendar: require('./model/calendar')(mongoose)
   };
-};
+
