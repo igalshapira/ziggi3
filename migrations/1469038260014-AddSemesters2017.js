fc5475a6fc2416e8a26fb55627a045c57976a3a1
Igal Shapira <igal.shapira@gmail.com>
2016-11-26 20:03:07 +0200

    Fix typo in migration. Didn't bother with new migration, nothing was ever deployed

--- migrations/1469038260014-AddSemesters2017.js
+++ migrations/1469038260014-AddSemesters2017.js
@@ -4,7 +4,7 @@ var db = require('../app/db');
 exports.up = function(next) {
   db.Semester.create([
   	{
-  		name: 'סתיז תשע"ז', year: 2017, semester: 1,
+  		name: 'סתיו תשע"ז', year: 2017, semester: 1,
   		startDate: new Date("2016-10-29T00:00:00.000Z"),
   		endDate: new Date("2017-01-26T00:00:00.000Z"),
   		examsStart: new Date("2017-01-28T00:00:00.000Z"),
