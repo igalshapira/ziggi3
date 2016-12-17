d464e006bcbb24e3e24da3483a32e3a3503887fa
Igal Shapira <igal.shapira@gmail.com>
2016-12-17 22:00:31 +0200

    Add building API and rooms to buildings

--- app/api/rooms.js
+++ app/api/rooms.js
@@ -15,6 +15,26 @@ module.exports = function(app) {
                     return response.status(500).send();
                 response.json(buildings);
             });
-        });
+    });
+
+    /**
+     * @apiGroup building
+     * @apiName Get building information
+     * @apiVersion 3.0.0
+     * @api {get} buildings Show list of available buildings
+     * @apiParam {string} name Building name
+     * @apiSuccess {} building data
+     * @apiError BuildingNotFound
+     */
+    app.get('/api/v3/buildings/:name', function(request, response) {
+        var name = request.params.name;
+        app.settings.db.Building.find(
+            { "name" : new RegExp(name, 'i') },
+            { _id: 0, __v: 0 },
+            function(error, building) {
+                if (error || building.length == 0)
+                    return response.status(404).send("BuildingNotFound");
+                response.json(building[0]);
+            });
     });
 };
--- app/model/building.js
+++ app/model/building.js
@@ -1,9 +1,16 @@
 module.exports = function(mongoose) {
+	var roomsSchema = mongoose.Schema({
+		name: String,
+		floor: Number,
+		comments: String
+	});
+
 	var buildingSchema = mongoose.Schema({
         name: String,
         lat: Number,
         lng: Number,
-        comments: String
+        comments: String,
+        rooms: [roomsSchema]
     });
 
     return mongoose.model('Building', buildingSchema);
